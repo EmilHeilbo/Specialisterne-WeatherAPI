f2340c055e0e243a8432917536d509a1cd3b6107
Emil Heilbo <Emil.Heilbo@specialisterne.dk>
2026-03-27 10:03:44 +0100

    chore(dbcontext): try to make context read-only without a read-only db user

--- Specialisterne-WeatherAPI/Context/DmiDbContext.cs
+++ Specialisterne-WeatherAPI/Context/DmiDbContext.cs
@@ -3,12 +3,12 @@ using Specialisterne_WeatherAPI.DTOs;
 
 namespace Specialisterne_WeatherAPI.Context;
 
-public class DmiDbContext(DbContextOptions<DmiDbContext> options) : DbContext(options)
+public sealed class DmiDbContext(DbContextOptions<DmiDbContext> options) : DbContext(options)
 {
-    public virtual DbSet<Bme280> Bme280 { get; set; }
-    public virtual DbSet<Dmi> Dmi { get; set; }
-    public virtual DbSet<Ds18B20> Ds18B20 { get; set; }
-    public virtual DbSet<Scd41> Scd41 { get; set; }
+    public DbSet<Bme280> Bme280 { get; set; }
+    public DbSet<Dmi> Dmi { get; set; }
+    public DbSet<Ds18B20> Ds18B20 { get; set; }
+    public DbSet<Scd41> Scd41 { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -19,5 +19,33 @@ public class DmiDbContext(DbContextOptions<DmiDbContext> options) : DbContext(op
         var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "herodot_etl";
 
         optionsBuilder.UseNpgsql($"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword}");
+    }
+
+    [Obsolete("DB Context is read-only",true)]
+#pragma warning disable CS0809
+    public override int SaveChanges()
+    {
+        throw new InvalidOperationException("This context is read-only.");
+    }
+
+    [Obsolete("DB Context is read-only",true)]
+#pragma warning disable CS0809
+    public override int SaveChanges(bool acceptAll)
+    {
+        throw new InvalidOperationException("This context is read-only.");
+    }
+
+    [Obsolete("DB Context is read-only",true)]
+    #pragma warning disable CS0809
+    public override Task<int> SaveChangesAsync(CancellationToken token = default)
+    {
+        throw new InvalidOperationException("This context is read-only.");
+    }
+
+    [Obsolete("DB Context is read-only",true)]
+    #pragma warning disable CS0809
+    public override Task<int> SaveChangesAsync(bool acceptAll, CancellationToken token = default)
+    {
+        throw new InvalidOperationException("This context is read-only.");
     }
 }
